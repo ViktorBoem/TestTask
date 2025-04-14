df691c0fd1704d4f2b4eaff589fec815a7256f66
ViktorBoem <vashchukvitja@gmail.com>
2025-04-14 23:59:04 +0300

    initial commit

---
+++ app/src/main/java/com/example/testtask/ui/theme/Color.kt
@@ -0,0 +1,19 @@
+package com.example.testtask.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val White = Color(0xFFFFFFFF)
+val Dark = Color(0xFF000000)
+val UranianBlue = Color(0xFFB2DEFB)
+val LightRed = Color(0xFFFF6B6B)
+val Aquamarine = Color(0xFF1CE4B5)
+val RobinEggBlue = Color(0xFF21D7E2)
+val DimGray = Color(0xFF707070)
