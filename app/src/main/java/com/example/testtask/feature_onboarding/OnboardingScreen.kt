54097701d89367e7e308a92772c9dd1d00e01c87
ViktorBoem <vashchukvitja@gmail.com>
2025-04-15 19:14:39 +0300

    add fonts and image

--- app/src/main/java/com/example/testtask/feature_onboarding/OnboardingScreen.kt
+++ app/src/main/java/com/example/testtask/feature_onboarding/OnboardingScreen.kt
@@ -1,7 +1,6 @@
 package com.example.testtask.feature_onboarding
 
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -12,22 +11,12 @@ import androidx.compose.ui.Modifier
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material3.Text
 import androidx.compose.ui.Alignment
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.pager.HorizontalPager
-import androidx.compose.foundation.pager.PagerState
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.pointer.motionEventSpy
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import com.example.testtask.R
 import com.example.testtask.components.HeightSizedSquareWithRoundedBottom
 import kotlinx.coroutines.launch
 import com.example.testtask.feature_onboarding.component.*
